96a8fe2d010c9d8792cdc3fb856c7f5b163482ec
Elin Anna Topp <Elin_Anna.Topp@cs.lth.se>
2025-02-10 15:43:22 +0100

    made use of Filters.py mandatory

--- assignment2/handout2025/Filters.py
+++ assignment2/handout2025/Filters.py
@@ -15,13 +15,26 @@ class HMMFilter:
         self.__sm = sm
         self.__f = probs
         
-        
-    def filter(self, sensorR) :
+    # sensorR is the sensor reading (index!), self._f is the probability distribution resulting from the filtering    
+    def filter(self, sensorR : int) -> np.array :
         #print( self.__f)
         
         #...
         return self.__f
 
-        
-        
-        
+
+class HMMSmoother:
+    
+    def __init__(self, tm, om, sm):
+        self.__tm = tm
+        self.__om = om
+        self.__sm = sm
+
+    # sensor_r_seq is the sequence (array) with the t-k sensor readings for smoothing, 
+    # f_k is the filtered result (f_vector) for step k
+    # fb is the smoothed result (fb_vector)
+    def smooth(self, sensor_r_seq : np.array, f_k : np.array) -> np.array:
+        fb = self.__f # setting a dummy value here...
+        # somehow compute fb to be better than f ;-)
+        # ...
+        return fb
